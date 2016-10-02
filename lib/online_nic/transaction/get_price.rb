6724d762ec4a7c94cfb1038ec9f80b79d9c84b89
Fred Snyder <fsnyder@castwide.com>
2016-10-02 18:24:57 -0400

    Period parameter for get_price transactions.

--- lib/online_nic/transaction/get_price.rb
+++ lib/online_nic/transaction/get_price.rb
@@ -2,14 +2,15 @@ module OnlineNic
   class Transaction::GetPrice < Transaction::Base
     def process_request
       domain = config[:domain]
-      op = config[:op]
+      op = config[:op] || 'reg'
+      period = config[:period] || 1
       domaintype = DomainExtensions.get_type(domain)
       request = create_request 'domain', 'GetDomainPrice'
       request.add_param 'domaintype', domaintype
       request.add_param 'domain', domain
       request.add_param 'op', op
-      request.add_param 'period', 1
-      request.set_checksum 'getdomainprice', domaintype, domain, op, 1
+      request.add_param 'period', period
+      request.set_checksum 'getdomainprice', domaintype, domain, op, period
       send_data request
     end
     def process_response
